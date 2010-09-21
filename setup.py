a0784044107f78b04dda9b65c3eaabead6fd325e
vonhollen <nicholas.vonhollen@rackspace.com>
2010-09-21 15:57:12 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils import setup
+from setuptools import setup
 
 setup(
 		name="python-daemonhelper",
 		version="0.1",
-		author="Nicholas VonHollen"
+		author="Nicholas VonHollen",
 		author_email="nicholas.vonhollen@rackspace.com",
 		license="Apache License 2.0",
 		packages=['daemonhelper'],
